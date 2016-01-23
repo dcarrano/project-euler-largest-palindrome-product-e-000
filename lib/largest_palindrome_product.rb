9393f9f9d8f341313a26f0fbda945c9b6c7eb22b
dcarrano <dcarrano@gmail.com>
2016-01-23 23:25:23 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,25 @@
-# Implement your procedural solution here!
+def palindrome?(number)
+  if number.to_s == number.to_s.reverse then true else false end
+end
+
+def largest_palindrome_product
+
+  largest = 0
+
+  a = 999
+  b = 999
+
+  until (a == 1) && (b == 1)
+    product = a * b
+    if palindrome?(product) && product > largest then largest = product end
+    if b == 1 then
+        b = a
+        a -= 1
+       else b -= 1
+       end
+  end
+
+  largest
+
+end
+
